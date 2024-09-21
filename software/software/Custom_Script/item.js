9de0559e5666caf87521bd89b394b2b466dd57bd
sudamrathod <sudamrathod022@gmail.com>
2024-09-21 17:32:30 +0530

    it is a smart product

---
+++ software/software/Custom_Script/item.js
@@ -0,0 +1,23 @@
+frappe.ui.form.on("Item", {
+    onload: function(frm) {
+		// your code here
+		frm.set_query("custom_material_description", function() {
+            return {
+                filters: [
+                    ["Material Description","item_group","=", frm.doc.item_group]
+                ]
+            };
+        });
+        frm.set_query("custom_material_category", function() {
+            return {
+                filters: [
+                    ["Material Category","material_description", "=", frm.doc.custom_material_description]
+                ]
+            };
+        });
+		
+
+	}
+})   
+
+// __________________
---
+++ software/software/doctype/material_category/material_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, sudam and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MaterialCategory(Document):
+	pass
