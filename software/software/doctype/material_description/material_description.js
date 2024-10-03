04aee1b61049782c31b99d62b19561cf60c51a3f
sudamrathod <sudamrathod022@gmail.com>
2024-10-03 17:01:30 +0530

    sudam

--- software/software/doctype/material_description/material_description.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, sudam and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Material Description", {
-// 	refresh(frm) {
-
-// 	},
-// });
